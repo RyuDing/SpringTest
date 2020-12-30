23d5f3d66bf0ca00334e6128a94d6b9599c179af
sf0855 <sf085@company>
2020-12-30 14:35:27 +0900

    Command객체 이용 - @ModelAttribute - 자동주입

--- 15_CommandObject/WebContent/WEB-INF/views/test1.jsp
+++ 15_CommandObject/WebContent/WEB-INF/views/test1.jsp
@@ -8,8 +8,10 @@
 </head>
 <body>
 	<h2>test1.jsp</h2>
-	<div>data1 : ${dataBean.data1}</div>
-	<div>data2 : ${dataBean.data2}</div>
-	<a href="/"></a>
+	<div>data1(dataBean:넘어오는 name을 지정 안했을 때) : ${dataBean.data1}</div>
+	<div>data2(dataBean:넘어오는 name을 지정 안했을 때) : ${dataBean.data2}</div>
+	<div>data1(zz:넘어오는 name을 지정 했을 때) : ${zz.data1}</div>
+	<div>data2(zz:넘어오는 name을 지정 했을 때) : ${zz.data2}</div>
+	<a href="/">>index.jsp<</a>
 </body>
 </html>
--- 15_CommandObject/src/kr/co/softcampus/controller/TestController.java
+++ 15_CommandObject/src/kr/co/softcampus/controller/TestController.java
@@ -1,5 +1,7 @@
 package kr.co.softcampus.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,14 +10,14 @@ import kr.co.softcmpus.beans.DataBean;
 
 @Controller
 public class TestController {
-	
+	public static Logger logger = LoggerFactory.getLogger(TestController.class);
 	// 자동 주입
 	@PostMapping("/test1")
-	public String test1(@ModelAttribute DataBean bean1) {
+	// ModelAttribute는 생략 가능.
+	public String test1(@ModelAttribute(name = "zz") DataBean bean1) { // DataBean -> dataBean으로 jsp에 전달됨.
 		System.out.println("[data1]"+bean1.getData1());
 		System.out.println("[data2]"+bean1.getData2());
 		
 		return "test1"; 
 	}
-	
 }
